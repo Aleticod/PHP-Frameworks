1fa0569885846fb1bb0a99d958cd8e672861fad7
me <193837@unsaac.edu.pe>
2022-09-09 18:02:07 +0200

    :sparkles: Add request and index code

--- app/Http/Request.php
+++ app/Http/Request.php
@@ -11,6 +11,10 @@ class Request {
         // https://platzi.com/servicios/index
 
         $this->segments = explode('/', $_SERVER['REQUEST_URI']);
+        
+        // var_dump($_SERVER['REQUEST_URI']);
+        // echo "<br>";
+        // var_dump($this->segments);
 
         $this->setController();
         $this->setMethod();
@@ -26,5 +30,23 @@ class Request {
         $this->method = empty($this->segments[2]) 
             ? 'index' 
             : $this->segments[2];
+    }
+
+    public function getController() {
+        // home -> HomeController
+        $controller = ucfirst($this->controller);
+        return "App\Http\Controllers\\{$controller}Controller";
+    }
+
+    public function getMethod() {
+        return $this->method;
+    }
+
+    public function send() {
+        $controller = $this->getController();
+        $method = $this->getMethod();
+
+        $response = call_user_func([new $controller, $method]);
+        $response->send();
     }
 }
